f93a92ab544d5fa1d842574f62aed281a78266b1
Bogdan282 <bogdan673876@gmail.com>
2025-05-22 11:36:02 +0300

    Add Complete Task

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,7 @@ public class Main {
         DataRepository repository = new DataRepository();
         DataHandler handler = new DataHandler();
         UIOperator uiOperator = new UIOperator();
+
         uiOperator.getOutput(handler.formListOutput(repository.getData()));
         uiOperator.getOutput(handler.formOutput(repository.getData(), 2));
     }
